fea791fe73953ba10943c2bdf838c87d6d43befc
duskcus <60875989+duskcus@users.noreply.github.com>
2025-01-09 16:07:45 +0100

    Update CrudTable.php

--- app/Livewire/website/CrudTable.php
+++ app/Livewire/website/CrudTable.php
@@ -3,9 +3,19 @@
 namespace App\Livewire\Website;
 
 use Livewire\Component;
+use App\Models\User;
 
 class CrudTable extends Component
 {
+    public function mount()
+    {
+        // Get all users
+        $users = User::all();
+
+        // Return a view with users data
+        return view('users.index', compact('users'));
+    }
+
     public function render()
     {
         return view('livewire.website.crud-table');
