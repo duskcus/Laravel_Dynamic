29e9f146103b0d40c5f0570b83341a5aa228e36d
duskcus <60875989+duskcus@users.noreply.github.com>
2025-01-14 15:18:22 +0100

    Update Footer.php

--- app/Livewire/Footer.php
+++ app/Livewire/Footer.php
@@ -6,11 +6,19 @@ use Livewire\Component;
 
 class Footer extends Component
 {
-    public $sections = [];
+    public $sections;
+    public $footer1;
+    public $footer2;
+
+    public function mount($sections, $footer1, $footer2)
+    {
+        $this->sections = $sections;
+        $this->footer1 = $footer1;  // Assign the footer1 state
+        $this->footer2 = $footer2;  // Assign the footer2 state
+    }
 
     public function render()
     {
-        // Pass the sections to the view
         return view('livewire.footer');
     }
 }
