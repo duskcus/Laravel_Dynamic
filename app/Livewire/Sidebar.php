a2567354eb8c9f5720e3a6580df717adec9f42e0
duskcus <60875989+duskcus@users.noreply.github.com>
2025-01-14 14:40:22 +0100

    Update Sidebar.php

--- app/Livewire/Sidebar.php
+++ app/Livewire/Sidebar.php
@@ -6,18 +6,10 @@ use Livewire\Component;
 
 class Sidebar extends Component
 {
+    public $sections = [];
+
     public function render()
     {
-        // Dynamically generate the navbar items
-        $sidebarItems = [
-            ['name' => 'Home', 'url' => route('home')],
-            ['name' => 'Users', 'url' => route('users.index')],
-            ['name' => 'About', 'url' => route('about')],
-            ['name' => 'Services', 'url' => route('services')],
-            ['name' => 'Contact', 'url' => route('contact')],
-        ];
-        
-        // Pass the items to the view
-        return view('livewire.sidebar', compact('sidebarItems'));
+        return view('livewire.sidebar');
     }
 }
