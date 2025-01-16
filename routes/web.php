b2a964bb455305fff4274236530d83804176729d
duskcus <60875989+duskcus@users.noreply.github.com>
2025-01-16 13:51:12 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,14 @@ Route::get('/', function () {
     return view('index');
 })->name('home');
 
+Route::get('/api', function () {
+    return view('api.index');
+})->name('api');
+
+Route::get('/form', function () {
+    return view('form.index');
+})->name('form');
+
 Route::get('/placeholder', function () {
     return ('placeholder route');
 })->name('placeholder');
